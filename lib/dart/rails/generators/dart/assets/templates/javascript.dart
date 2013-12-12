207e09772aeb3f69021ffb08b4aa0ab95e74fd58
Marcel Sackermann <marcel@iras-revier.de>
2013-12-12 21:21:01 +0100

    Version 0.0.2:   -added generator dart:assets   -added support for `rake pub:get`

--- lib/dart/rails/generators/dart/assets/templates/javascript.dart
+++
@@ -1,9 +0,0 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available as <%= file_name %>.js if you
-// created the <%= file_name %>.js.dart2js template and //= include <%= file_name %>
-// You can use Dart in this file: http://dartlang.org/
-
-import ''
-
-void main() {
-}
